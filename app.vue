fccd970183320594ba8beaaf1f1d466eed0f3afc
Boonraksa Katawundee <myboonraksa12@gmail.com>
2024-01-17 01:00:54 +0700

    init

---
+++ app.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+const name = "mind";
+const fruit = [ 'apple','banana','melon']
+</script>
+
+
+
+<template>
+  <div class="text-red-500">
+    Hello World{{ name }}
+    <div v-for="item of fruit">{{ item }}</div>
+  </div>
+</template>
