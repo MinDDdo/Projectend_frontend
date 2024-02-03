52cc7093e93eb5e8b94449ef6a927f03262f36c0
MinDDdo <94242773+MinDDdo@users.noreply.github.com>
2024-02-04 01:18:13 +0700

    update teacher

--- interfaces/teacher.interface.ts
+++ interfaces/teacher.interface.ts
@@ -1,8 +1,17 @@
-export interface TeachSigupDto {
+export interface TeacherSigupDto {
     firstname: string;
     lastname: string;
     dob: string;
     gender: string;
     email: string;
     password: string;
+}
+
+export interface TeacherUpdateDto {
+    firstname?: string; 
+    lastname?: string;
+    dob?: string; 
+    gender?: string; 
+    line_contact?: string; 
+    phone_contact?: string;
 }
--- services/teacher.ts
+++ services/teacher.ts
@@ -1,12 +1,12 @@
 import axios, { AxiosError } from 'axios';
-import type { TeachSigupDto } from '~/interfaces/teacher.interface';
+import type { TeacherSigupDto, TeacherUpdateDto } from '~/interfaces/teacher.interface';
 
 
-export const signup = async (data: TeachSigupDto) => {
+export const signup = async (data: TeacherSigupDto) => {
     try {
         const response = await axios({
             method: 'post',
-            url: "http://localhost:8080/v1/auth/login",
+            url: "http://localhost:8080/v1/teacher/signup",
             data: {
                 firstname: data.firstname,
                 lastname: data.lastname,
@@ -27,8 +27,35 @@ export const signup = async (data: TeachSigupDto) => {
     } 
 }
 
-const teacherService = {
-    signup
+export const update = async (teacherId: string ,data: TeacherUpdateDto) => {
+    try {
+        const response = await axios({
+            method: 'post',
+            url: "http://localhost:8080/v1/teacher/update/" + teacherId,
+            data: {
+                firstname: data.firstname, 
+                lastname: data.lastname,
+                dob: data.dob, 
+                gender: data.gender, 
+                line_contact: data.line_contact, 
+                phone_contact: data.phone_contact
+            }
+        })
+
+        return response.data;
+    }catch (error){
+        if (error instanceof AxiosError) {
+            return error.response?.data;
+        }
+
+        return 'Something went wrong';
+    } 
 }
+
+const teacherService = {
+    signup,
+    update
+}
+
 
 export default teacherService;
