1e0e80bc5c31d8b428eeaeff4fd8712faac013c7
TOONWRY <102649393+TOONWRY@users.noreply.github.com>
2024-02-12 00:58:38 +0700

    html-edit Createclassroom

--- pages/Createclassroom.vue
+++ pages/Createclassroom.vue
@@ -3,8 +3,8 @@
 </script>
 <template>
     <div class="h-screen bg-[#EEF5FF] ">
-        <div class="flex justify-between px-10  ">
-            <div class="mt-3 pt-4">
+        <div class="flex justify-between px-10 ">
+            <div class="mt-5">
                 <img src="~/assets/images/menu.png" alt="menu" />
             </div>
             
@@ -12,36 +12,33 @@
                 <img src="~/assets/images/T1.png" alt="T1"/>
                 <p>ใจดี มีชัย</p>
             </div>
-            
         </div>
         <div class="mt-5 flex justify-center ">
-            <div class=" flex flex-col gap-y-32 items-center
-                         min-w-[550px] max-w-[550px] 
-                         min-h-[560px] max-h-[560px] 
-                         mt-10">
+            <div class="  ">
                 
                 <div class="flex justify-center relative">
-                    <div class=" flex   bg-white min-w-[670px] max-w-[670px]
-                                 min-h-[680px] max-h-[680px] rounded-[20px]  ">
+                    <div class=" flex  bg-white 
+                                w-[600px] 
+                                h-[570px] rounded-[20px]  ">
                                  <div class=" flex flex-col gap-y-5 items-center 
-                                 min-w-[670px] max-w-[670px]
-                                 min-h-[680px] max-h-[680px] rounded-[20px]">
+                                 w-[600px] 
+                                 h-[570px] rounded-[20px]">
                 
-                <div class="flex justify-center relative pt-20">
+                        <div class="flex justify-center relative pt-10">
                     <div class="  ">
                    
-                        <img src="~/assets/images/grey.png" alt="Kid (24)" width="190" height="190" class="rounded-full"  />
+                        <img src="~/assets/images/grey.png" alt="grey" width="160" height="160" class="rounded-full"  />
                 
                     </div>
 
                     <div class="absolute bottom-2 right-2">
                 
-                        <img src="~/assets/images/logoeditprofile.png" alt="logoeditprofile" width="40" height="40" class="rounded-full"  />
+                        <img src="~/assets/images/logoeditprofile.png" alt="logoeditprofile" width="30" height="40" class="rounded-full"  />
                 
                      </div>
                 </div>
                 
-                <form class="flex flex-col mt-8 gap-y-7 w-[500px]">
+                <form class="flex flex-col mt-2 gap-y-5 w-[450px]">
                     <input 
                         type="text" 
                         placeholder="ชื่อชั้นเรียน" 
@@ -65,12 +62,12 @@
                     <div class="flex justify-between h-[50px]  ">
                         <button 
                             type="submit" 
-                            class="p-2 font-bold bg-[#E5E5E5] rounded-xl w-[240px]">
+                            class="p-2 font-bold bg-[#E5E5E5] rounded-xl w-[210px]">
                             ยกเลิก
                         </button>
                         <button 
                             type="submit" 
-                            class="p-2 text-white font-bold bg-[#676B7D] rounded-xl w-[240px]">
+                            class="p-2 text-white font-bold bg-[#676B7D] rounded-xl w-[210px]">
                             สร้าง
                         </button>
                     </div>
@@ -85,10 +82,10 @@
                     </div>
 
                     <div class="absolute  -top-9
-                    bg-[#7071E8] text-white p-2
-                    min-w-[300px] max-w-[300px] 
-                    min-h-[65px] max-h-[65px] rounded-[20px] 
-                     text-[26px] px-20
+                    bg-[#7071E8] text-white p-3
+                    w-[250px] 
+                    h-[px] rounded-[20px] 
+                     text-[24px] px-[60px]
                     ">
                         <p>สร้างชั้นเรียน</p>
                         
