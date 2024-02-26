f54f5c810045f44551cc88d5df259516bd23a640
MinDDdo <94242773+MinDDdo@users.noreply.github.com>
2024-02-27 00:18:14 +0700

    add integrate updateteacher

--- pages/Updateteacher.vue
+++ pages/Updateteacher.vue
@@ -1,4 +1,55 @@
-<script>
+<script setup lang="ts">
+
+import type { TeacherResponse } from '@/interfaces/teacher.interface'
+import dayjs from 'dayjs';
+
+const teacher = ref<TeacherResponse | null>(null);
+
+const firstname = ref<string>('');
+const lastname = ref<string>('');
+const dob = ref<string>('');
+const gender = ref<string>('');
+const lineContact = ref<string>('');
+const phoneContact = ref<string>('');
+
+onMounted(async () => {
+    await getTeacherById();
+})
+
+const getTeacherById = async () => {
+    const data = await useApi.teacherService.getTeacherById('65c0f97b48f2c8d8846a2251')
+
+    if (!data) {
+            return navigateTo('/');
+        }
+
+        firstname.value = data.result.data.firstname;
+        lastname.value = data.result.data.lastname;
+        dob.value = dayjs(data.result.data.dob).format('YYYY-MM-DD HH:mm:ss');
+        gender.value = data.result.data.gender;
+        lineContact.value = data.result.data.line_contact;
+        phoneContact.value = data.result.data.phone_contact;
+        console.log(data.result.data)
+        
+}
+
+const updateTeacher = async () => {
+    const data = await useApi.teacherService.update('65c0f97b48f2c8d8846a2251', {
+        firstname: firstname.value,
+        lastname: lastname.value,
+        dob: dob.value,
+        gender: gender.value,
+        line_contact: lineContact.value,
+        phone_contact: phoneContact.value
+
+    })
+
+    if (!data) {
+            return navigateTo('/');
+        }
+
+        navigateTo('/classroomlist')
+}
 
 </script>
 
@@ -16,7 +67,9 @@
         </div>
 
         <div class="flex justify-center relative mt-5">
-            <div class="p-2 bg-[#FFFFFF] w-[530px] h-[530px] rounded-[30px] ">
+            <form 
+                @submit.prevent="updateTeacher"
+                class="p-2 bg-[#FFFFFF] w-[530px] h-[530px] rounded-[30px] ">
                 <div class="absolute left-1/2 transform -translate-x-1/2 -top-[60px]">
                     <img src="~/assets/images/T2.png" alt="T2"
                     class="w-[130px]"
@@ -25,67 +78,73 @@
 
                 <div class="flex justify-center mt-20">
                     <p class="mt-1 pr-5">ชื่อจริง</p>
-                    <form>
+                    
                         <input 
                             type="text"
                             placeholder="ชื่อ"
+                            v-model="firstname"
                             class="pt-1 bg-[#DCF2F1] rounded-[10px] w-[300px] h-[35px] pl-5"
                         />
-                    </form>
+                    
                 </div>
 
                 <div class="flex justify-center mt-7">
                     <p class="mt-1 pr-3">นามสกุล</p>
-                    <form>
+                    
                         <input 
                             type="text"
                             placeholder="นามสกุล"
+                            v-model="lastname"
                             class="pt-1 bg-[#DCF2F1] rounded-[10px] w-[300px] h-[35px] pl-5"
                         />
-                    </form>
+                    
                 </div>
 
                 <div class="flex justify-center mt-7">
                     <p class="mt-1 pr-6">วันเกิด</p>
-                    <form>
+                    
                         <input 
-                            type="date"
+                            type="input"
                             placeholder="วว/ดด/ปป"
+                            v-model="dob"
                             class="pt-1 bg-[#DCF2F1] rounded-[10px] w-[300px] h-[35px] pl-5"
                         />
-                    </form>
+                    
                 </div>
                 <div class="flex justify-center mt-7">
                     <p class="mt-1 pr-6">วันเกิด</p>
-                    <form>
+                    
                         <input 
                             type=""
                             placeholder="วว/ดด/ปป"
+                            
                             class="pt-1 bg-[#DCF2F1] rounded-[10px] w-[300px] h-[35px] pl-5"
                         />
-                    </form>
+                    
                 </div>
 
                 <div class="flex justify-center mt-7">
                     <p class="mt-1 pr-6">เบอร์</p>
-                    <form>
+                    
                         <input 
                             type="text"
                             placeholder="เบอร์"
+                            v-model="phoneContact"
                             class="pt-1 bg-[#DCF2F1] rounded-[10px] w-[300px] h-[35px] pl-5"
                         />
-                    </form>
+                    
                 </div>
 
                 <div class="flex justify-center mt-7">
                     <p class="mt-1 pr-5">ไอดีไลน์</p>
-                    <form>
+                    
                         <input 
                             type="text"
                             placeholder="ไอดีไลน์"
+                            v-model="lineContact"
                             class="pt-1 bg-[#DCF2F1] rounded-[10px] w-[300px] h-[35px] pl-5"
                         />
-                    </form>
+                    
                 </div>
 
                 <div class="flex justify-center mt-7 gap-x-10">
@@ -93,11 +152,11 @@
                         ยกเลิก
                     </button>
 
-                    <button class="p-1 bg-[#676B7D] w-[160px] h-[35px] rounded-[10px] text-white font-bold">
+                    <button type="submit" class="p-1 bg-[#676B7D] w-[160px] h-[35px] rounded-[10px] text-white font-bold">
                         บันทึก
                     </button>
                 </div>
-            </div>
+            </form>
         </div>
     </div>
 </template>
