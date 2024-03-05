d3e8419d75aced3ac458e268c4e97f85c91b3e06
TOONWRY <102649393+TOONWRY@users.noreply.github.com>
2024-03-05 23:25:35 +0700

    อวตาครู

--- pages/Updateteacher.vue
+++ pages/Updateteacher.vue
@@ -164,4 +164,57 @@ const updateTeacher = async () => {
             </form>
         </div>
     </div>
+
+    
+    <div 
+        class="fixed bg-gray-400 bg-opacity-50 h-screen w-full z-[99999] top-0 right-0 left-0" 
+       
+    >
+    </div>
+
+    <div 
+        class="fixed bg-white rounded-[20px]  min-w-[900px] min-h-[700px] z-[999999] 
+        top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 " 
+       
+    >
+        <div class="overflow-y-scroll h-[450px] w-[700px] mt-[80px] mx-[100px] grid grid-cols-5 gap-1" >
+            <img src="~/assets/images/T_1.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_2.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_3.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_4.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_5.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_6.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_7.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_8.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_9.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_10.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_11.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_12.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_13.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_14.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_15.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_16.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_17.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_18.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_19.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_20.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_21.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_22.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_23.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_24.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_25.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_25.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_27.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_28.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_29.png" alt="menu" class=" rounded-lg"  />
+            <img src="~/assets/images/T_30.png" alt="menu" class=" rounded-lg"  /> 
+        </div>
+        <div class=" grid justify-items-center pt-5" >
+            <button type="submit" class="p-2 bg-[#676B7D] w-[150px] h-[50px] rounded-[10px] text-white font-bold">
+                        บันทึก
+            </button>
+        </div>
+      
+    </div>
+
 </template>
