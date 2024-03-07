a6b82a5ff4f47454376b9ad9f3e48af37e08e2a9
MinDDdo <94242773+MinDDdo@users.noreply.github.com>
2024-03-07 15:54:22 +0700

    สร้างหน้า Teachercheckname

---
+++ pages/Teachercheckname.vue
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+
+<template>
+    <div class="min-h-screen bg-[#EEF5FF]">
+        <div class="flex justify-between px-10">
+            <div class="mt-5">
+                <img src="~/assets/images/arrow.png"  alt="arrow"
+                class=" pl-3 w-[75px]"
+                />
+            </div>
+            <div class="">
+                <div class="bg-white flex gap-x-5 items-center mt-3 p-1 rounded-[10px] w-[190px] ">
+                    <img src="~/assets/images/T1.png" alt="T1"/>
+                    <p>ใจดี มีชัย</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="flex justify-center">
+            <div class=" bg-[#B598CD] w-[280px] h-[55px] rounded-[15px]">
+                <p class="text-white font-bold text-2xl text-center mt-3">เช็คชื่อ</p>
+            </div>
+
+            
+        </div>
+    </div>
+</template>
